b531ebe9af117b07d2884a6d21cbd2b055d17c6d
Daniel Araujo <danielaraujo.github@gmail.com>
2023-05-22 22:44:10 -0300

    Commit message

--- ex_05.py
+++ ex_05.py
@@ -2,4 +2,12 @@
 # número sorteado. Se o usuário acertar, exibir o número de tentativas necessárias para o acerto. Se não
 # acertar após as 10 tentativas, exibir o número sorteado.
 
-for _ in randrange(100):
+import random
+numeroAleatorio = random.randrange(10)
+
+numeroInformado = int(input("Foi gerado um número aleatório entre 0 a 10, tente acertá-lo.\n Informe um número: "))
+
+if numeroInformado == numeroAleatorio:
+    print("Acertou! Meus parabéns!")
+else:
+    print(f"Errou! O número aleatório era {numeroAleatorio}. Tente novamente.")
